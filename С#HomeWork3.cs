c0cf874b4bbb1b04678710d6f79b94eb0c3da3e1
Artem <ar.toropov@mail.ru>
2022-12-07 23:35:53 +0300

    distance in between 2 poins in 3D

---
+++ С#HomeWork3.cs
@@ -0,0 +1,15 @@
+﻿void Distance(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+ double length = Math.Sqrt((Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2)) + Math.Pow((zA - zB),2));
+ Console.WriteLine($"distance between points is {Math.Round(length, 3)}");
+}
+
+Console.WriteLine("input coordinates in the following order: xA, yA, zA, xB, yB, zB");
+double xA = Convert.ToDouble(Console.ReadLine());
+double yA = Convert.ToDouble(Console.ReadLine());
+double zA = Convert.ToDouble(Console.ReadLine());
+double xB = Convert.ToDouble(Console.ReadLine());
+double yB = Convert.ToDouble(Console.ReadLine());
+double zB = Convert.ToDouble(Console.ReadLine());
+
+Distance(xA, yA, zA, xB, yB, zB);
