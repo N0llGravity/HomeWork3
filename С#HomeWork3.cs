cb79d5a45b705ddd6a18b1bde79e007eb5e08477
Artem <ar.toropov@mail.ru>
2022-12-11 23:29:09 +0300

    First task started

--- С#HomeWork3.cs
+++ С#HomeWork3.cs
@@ -1,15 +1,29 @@
-﻿void Distance(double xA, double yA, double zA, double xB, double yB, double zB)
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+itn ArrayEntry(int number)
 {
- double length = Math.Sqrt((Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2)) + Math.Pow((zA - zB),2));
- Console.WriteLine($"distance between points is {Math.Round(length, 3)}");
+    
 }
 
-Console.WriteLine("input coordinates in the following order: xA, yA, zA, xB, yB, zB");
-double xA = Convert.ToDouble(Console.ReadLine());
-double yA = Convert.ToDouble(Console.ReadLine());
-double zA = Convert.ToDouble(Console.ReadLine());
-double xB = Convert.ToDouble(Console.ReadLine());
-double yB = Convert.ToDouble(Console.ReadLine());
-double zB = Convert.ToDouble(Console.ReadLine());
 
-Distance(xA, yA, zA, xB, yB, zB);
+
+
+
+
+
+/* //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// void Distance(double xA, double yA, double zA, double xB, double yB, double zB)
+// {
+//  double length = Math.Sqrt((Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2)) + Math.Pow((zA - zB),2));
+//  Console.WriteLine($"distance between points is {Math.Round(length, 3)}");
+// }
+
+// Console.WriteLine("input coordinates in the following order: xA, yA, zA, xB, yB, zB");
+// double xA = Convert.ToDouble(Console.ReadLine());
+// double yA = Convert.ToDouble(Console.ReadLine());
+// double zA = Convert.ToDouble(Console.ReadLine());
+// double xB = Convert.ToDouble(Console.ReadLine());
+// double yB = Convert.ToDouble(Console.ReadLine());
+// double zB = Convert.ToDouble(Console.ReadLine());
+
+// Distance(xA, yA, zA, xB, yB, zB);
+*/
