9a1a954ed7a7807080200b647c5aedf5ad3ca2ee
Artem <ar.toropov@mail.ru>
2022-12-12 03:16:43 +0300

    Filling array ready

--- С#HomeWork3.cs
+++ С#HomeWork3.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
-int NumberLenght(int number)
+int NumberLength(int number)
 {
-int count = 0;
+    int count = 0;
 
     while (number > 0)
     {
@@ -11,19 +11,40 @@ int count = 0;
     return count;
 }
 
-int[] ArrayFilling(int[] Array, int size)
+int[] ArrayFilling(int[] array, int size, int number)
 {
-
+    int index = 0;
+    while(index < size)
+    {
+        array[index] = number % 10;
+        number = number / 10;
+        index++;
+    }
+    return array;
 }
 
-int Num = Convert.ToInt32(Console.ReadLine());
-int lenght = NumberLenght(Num);
+int []Arr
+Console.WriteLine("Input a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int length = NumberLength(num);
+if (length == 1)
+{
+    Console.WriteLine("True");
+}
+
+else
+{
+int[] array = new int[length];
+array = ArrayFilling(array, length, num);
+}
+
 
 
 
 
 
 /* //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
 // void Distance(double xA, double yA, double zA, double xB, double yB, double zB)
 // {
 //  double length = Math.Sqrt((Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2)) + Math.Pow((zA - zB),2));
